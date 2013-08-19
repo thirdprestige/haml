ca290c839da80d01033fee8a40949ac279ed8af8
Nathaniel Jones <nj@thirdprestige.com>
2013-08-18 21:41:03 -0500

    Add Boxen links to Welcome message

---
+++ commands/welcome.rb
@@ -0,0 +1,15 @@
+class Welcome
+  def execute
+    speak(
+      "Invite Member to Google Apps: https://admin.google.com/thirdprestige.com/AdminHome#UserList:org=1pgvlh93ky0661", 
+      "Invite Member to Basecamp: https://basecamp.com/1854629/people/new", 
+      "Invite Member to Campfire: https://thirdprestige.campfirenow.com/invitations/new",
+      "Upgrade Harvest plan to support more users: https://thirdprestige.harvestapp.com/company/edit_plan", 
+      "Add Member to Harvest: https://thirdprestige.harvestapp.com/people",
+      "Add Member to GitHub: `haml authorize @HamlTheHamster",
+      "Add Member to Heroku: `haml collaborate haml@thirdprestige.com", 
+      "Ensure Member has signed a Sub-contractor agreement: https://na2.docusign.net/Member/EnvelopeSetup.aspx?ti=2a1554a1d86040d49aab723af894bcea&",
+      "Email links to https://boxen.thirdprestige.com, https://github.com/thirdprestige/boxen"
+    )
+  end
+end
